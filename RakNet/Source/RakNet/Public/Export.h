f8d60beafcc8f61770a8edc2691722c595304161
Yujen-PC <Yujen-PC>
2016-07-11 18:12:19 +0800

    Definition of DLLIMPORT function not allowed. #1

--- RakNet/Source/RakNet/Public/Export.h
+++ RakNet/Source/RakNet/Public/Export.h
@@ -12,9 +12,10 @@
 
 #if defined(_WIN32) && !(defined(__GNUC__)  || defined(__GCCXML__)) && !defined(_RAKNET_LIB) && defined(_RAKNET_DLL)
 #define RAK_DLL_EXPORT __declspec(dllexport) 
-#else
-//#define RAK_DLL_EXPORT RAKNET_API 
+#elif defined(_WIN64)
 #define RAK_DLL_EXPORT __declspec(dllexport) 
+#else
+#define RAK_DLL_EXPORT RAKNET_API 
 #endif
 
 #define STATIC_FACTORY_DECLARATIONS(x) static x* GetInstance(void); \
