12d7401e3d23d33b675cf31cc0f2ddd589887a96
Carlos Guerrero <carlos.disgaea@gmail.com>
2019-07-24 00:41:10 +0100

    now it isn't necesary to create an static mesh actor, to send data to orion server

--- Source/RN4UE4/ReplicaRigidDynamicClient.cpp
+++ Source/RN4UE4/ReplicaRigidDynamicClient.cpp
@@ -70,117 +70,122 @@ RigidDynamicConstructionData UReplicaRigidDynamicClient::GetConstructionData()
 
 	RigidDynamicConstructionData data;
 	data.geom = typeMesh;
-	FVector posi = actorTransform.GetLocation();
-	posi = posi / 50.0f;
-	data.pos.X = posi.X;
-	data.pos.Y = posi.Z;
-	data.pos.Z = posi.Y;
+	FVector position = actorTransform.GetLocation();
+	position = position / 50.0f;
+	data.pos.X = position.X;
+	data.pos.Y = position.Z;
+	data.pos.Z = position.Y;
 	data.rot.X = actorTransform.GetRotation().X;
 	data.rot.Y = actorTransform.GetRotation().Y;
 	data.rot.Z = actorTransform.GetRotation().Z;
 	data.rot.W = actorTransform.GetRotation().W;
-	AStaticMeshActor * vismesh = dynamic_cast<AStaticMeshActor*>(GetOwner());
-	if (vismesh != nullptr) {
-		data.sca.X = vismesh->GetActorScale().X;
-		data.sca.Y = vismesh->GetActorScale().Z;
-		data.sca.Z = vismesh->GetActorScale().Y;
-		data.forwardVector.X = vismesh->GetActorForwardVector().X;
-		data.forwardVector.Y = vismesh->GetActorForwardVector().Y;
-		data.forwardVector.Z = vismesh->GetActorForwardVector().Z;
-		data.mass = vismesh->GetStaticMeshComponent()->GetMass();
-		data.inertia.X = vismesh->GetStaticMeshComponent()->GetInertiaTensor().X;
-		data.inertia.Y = vismesh->GetStaticMeshComponent()->GetInertiaTensor().Y;
-		data.inertia.Z = vismesh->GetStaticMeshComponent()->GetInertiaTensor().Z;
-		data.angularDamping = vismesh->GetStaticMeshComponent()->GetAngularDamping();
-		data.linearDamping = vismesh->GetStaticMeshComponent()->GetLinearDamping();
-		data.gravityEnabled = vismesh->GetStaticMeshComponent()->IsGravityEnabled();
-
-		//if box
-		if (typeMesh == 3) {
-			FRotator rotAux = GetOwner()->GetActorRotation();
-			FVector ext;
-			FBoxSphereBounds sph;
-			FTransform tr;
-			GetOwner()->SetActorRotation(FQuat(0, 0, 0, 1));
-			sph = vismesh->GetStaticMeshComponent()->CalcBounds(tr);
-			data.extents.X = sph.BoxExtent.X;
-			data.extents.Y = sph.BoxExtent.Z;
-			data.extents.Z = sph.BoxExtent.Y;
-			GetOwner()->SetActorRotation(rotAux);
-		}
-
-		//if sphere
-		else if (typeMesh == 0) {
-			float radius;
-			FBoxSphereBounds sph;
-			FTransform tr;
-			vismesh->GetStaticMeshComponent()->GetBodySetup()->AggGeom.CalcBoxSphereBounds(sph, tr);
-			radius = sph.SphereRadius;
-			data.extents.X = radius;
-			data.extents.Y = 0.0f;
-			data.extents.Z = 0.0f;
-		}
-		//if capsule
-		else if (typeMesh == 2) {
-			FRotator rotAux = GetOwner()->GetActorRotation();
-			FVector ext;
-			FBoxSphereBounds sph;
-			FTransform tr;
-			GetOwner()->SetActorRotation(FQuat(0, 0, 0, 1));
-			vismesh->GetStaticMeshComponent()->GetBodySetup()->AggGeom.CalcBoxSphereBounds(sph, tr);
-			ext = sph.BoxExtent;
-			ext = ext * 0.5;
-			data.extents.X = ext.X;
-			data.extents.Y = ext.Y - ext.X;
-			data.extents.Z = 0.0f;
-			GetOwner()->SetActorRotation(rotAux);
-		}
-		//if mesh
-		else if (typeMesh == 4) {
-			data.numVertex = vismesh->GetStaticMeshComponent()->GetBodySetup()->AggGeom.ConvexElems[0].VertexData.Num();
-			for (FVector vec : vismesh->GetStaticMeshComponent()->GetBodySetup()->AggGeom.ConvexElems[0].VertexData)
+	TArray<UPrimitiveComponent*> comps;
+	GetOwner()->GetComponents(comps);
+	for (auto Iter = comps.CreateConstIterator(); Iter; ++Iter)
+	{
+		UStaticMeshComponent* vismesh = Cast<UStaticMeshComponent>(*Iter);
+			if (vismesh)
 			{
-				FVector aux = vec / 50.0f;
-				Vec3 ver;
-				ver.X = aux.X;
-				ver.Y = aux.Z;
-				ver.Z = aux.Y;
-				data.vertexData.push_back(ver);
+				data.sca.X = GetOwner()->GetActorScale().X;
+				data.sca.Y = GetOwner()->GetActorScale().Z;
+				data.sca.Z = GetOwner()->GetActorScale().Y;
+				data.forwardVector.X = GetOwner()->GetActorForwardVector().X;
+				data.forwardVector.Y = GetOwner()->GetActorForwardVector().Y;
+				data.forwardVector.Z = GetOwner()->GetActorForwardVector().Z;
+				data.mass = vismesh->GetMass();
+				data.inertia.X = vismesh->GetInertiaTensor().X;
+				data.inertia.Y = vismesh->GetInertiaTensor().Y;
+				data.inertia.Z = vismesh->GetInertiaTensor().Z;
+				data.angularDamping = vismesh->GetAngularDamping();
+				data.linearDamping = vismesh->GetLinearDamping();
+				data.gravityEnabled = vismesh->IsGravityEnabled();
+				//if box
+				if (typeMesh == 3) {
+					FRotator rotAux = GetOwner()->GetActorRotation();
+					FVector ext;
+					FBoxSphereBounds sph;
+					FTransform tr;
+					GetOwner()->SetActorRotation(FQuat(0, 0, 0, 1));
+					sph = vismesh->CalcBounds(tr);
+					data.extents.X = sph.BoxExtent.X;
+					data.extents.Y = sph.BoxExtent.Z;
+					data.extents.Z = sph.BoxExtent.Y;
+					GetOwner()->SetActorRotation(rotAux);
+				}
+
+				//if sphere
+				else if (typeMesh == 0) {
+					float radius;
+					FBoxSphereBounds sph;
+					FTransform tr;
+					vismesh->GetBodySetup()->AggGeom.CalcBoxSphereBounds(sph, tr);
+					radius = sph.SphereRadius;
+					data.extents.X = radius;
+					data.extents.Y = 0.0f;
+					data.extents.Z = 0.0f;
+				}
+				//if capsule
+				else if (typeMesh == 2) {
+					FRotator rotAux = GetOwner()->GetActorRotation();
+					FVector ext;
+					FBoxSphereBounds sph;
+					FTransform tr;
+					GetOwner()->SetActorRotation(FQuat(0, 0, 0, 1));
+					vismesh->GetBodySetup()->AggGeom.CalcBoxSphereBounds(sph, tr);
+					ext = sph.BoxExtent;
+					ext = ext * 0.5;
+					data.extents.X = ext.X;
+					data.extents.Y = ext.Y - ext.X;
+					data.extents.Z = 0.0f;
+					GetOwner()->SetActorRotation(rotAux);
+				}
+				//if mesh
+				else if (typeMesh == 4) {
+					data.numVertex = vismesh->GetBodySetup()->AggGeom.ConvexElems[0].VertexData.Num();
+					for (FVector vec : vismesh->GetBodySetup()->AggGeom.ConvexElems[0].VertexData)
+					{
+						FVector aux = vec / 50.0f;
+						Vec3 ver;
+						ver.X = aux.X;
+						ver.Y = aux.Z;
+						ver.Z = aux.Y;
+						data.vertexData.push_back(ver);
+					}
+				}
+
+				FVector centerMass = vismesh->GetCenterOfMass();
+				centerMass = centerMass / 50.0f;
+				data.centerMass.X = centerMass.X;
+				data.centerMass.Y = centerMass.Z;
+				data.centerMass.Z = centerMass.Y;
+				data.MaxAngularVelocity = vismesh->GetBodyInstance()->MaxAngularVelocity;
+				data.typeName = TCHAR_TO_UTF8(*vismesh->GetBodySetup()->GetName());
+				data.restitution = vismesh->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getRestitution();
+				PxCombineMode::Enum restituCombineMode = vismesh->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getRestitutionCombineMode();
+				data.dynamicFriction = vismesh->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getDynamicFriction();
+				data.staticFriction = vismesh->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getStaticFriction();
+				PxCombineMode::Enum frictionCombineMode = vismesh->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getFrictionCombineMode();
+				data.referenceCount = vismesh->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getReferenceCount();
+				//PxFlags<PxMaterialFlag::Enum, PxU16> flags = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getFlags();
+				if (restituCombineMode == PxCombineMode::eAVERAGE)
+					data.restitutionCombineMode = 0;
+				else if (restituCombineMode == PxCombineMode::eMIN)
+					data.restitutionCombineMode = 1;
+				else if (restituCombineMode == PxCombineMode::eMULTIPLY)
+					data.restitutionCombineMode = 2;
+				else if (restituCombineMode == PxCombineMode::eMAX)
+					data.restitutionCombineMode = 3;
+				if (frictionCombineMode == PxCombineMode::eAVERAGE)
+					data.frictionCombineModeInt = 0;
+				else if (frictionCombineMode == PxCombineMode::eMIN)
+					data.frictionCombineModeInt = 1;
+				else if (frictionCombineMode == PxCombineMode::eMULTIPLY)
+					data.frictionCombineModeInt = 2;
+				else if (frictionCombineMode == PxCombineMode::eMAX)
+					data.frictionCombineModeInt = 3;
+				//	constructionBitstream->Write<PxMaterialFlags>(flags);
 			}
-		}
-
-		FVector centerMass = vismesh->GetStaticMeshComponent()->GetCenterOfMass();
-		data.centerMass.X = centerMass.X;
-		data.centerMass.Y = centerMass.Z;
-		data.centerMass.Z = centerMass.Y;
-		data.MaxAngularVelocity = vismesh->GetStaticMeshComponent()->GetBodyInstance()->MaxAngularVelocity;
-		data.typeName = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getConcreteTypeName();
-		data.restitution = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getRestitution();
-		PxCombineMode::Enum restituCombineMode = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getRestitutionCombineMode();
-		data.dynamicFriction = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getDynamicFriction();
-		data.staticFriction = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getStaticFriction();
-		PxCombineMode::Enum frictionCombineMode = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getFrictionCombineMode();
-		data.referenceCount = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getReferenceCount();
-		//PxFlags<PxMaterialFlag::Enum, PxU16> flags = vismesh->GetStaticMeshComponent()->GetBodySetup()->GetPhysMaterial()->GetPhysXMaterial()->getFlags();
-		if (restituCombineMode == PxCombineMode::eAVERAGE)
-			data.restitutionCombineMode = 0;
-		else if (restituCombineMode == PxCombineMode::eMIN)
-			data.restitutionCombineMode = 1;
-		else if (restituCombineMode == PxCombineMode::eMULTIPLY)
-			data.restitutionCombineMode = 2;
-		else if (restituCombineMode == PxCombineMode::eMAX)
-			data.restitutionCombineMode = 3;
-		if (frictionCombineMode == PxCombineMode::eAVERAGE)
-			data.frictionCombineModeInt = 0;
-		else if (frictionCombineMode == PxCombineMode::eMIN)
-			data.frictionCombineModeInt = 1;
-		else if (frictionCombineMode == PxCombineMode::eMULTIPLY)
-			data.frictionCombineModeInt = 2;
-		else if (frictionCombineMode == PxCombineMode::eMAX)
-			data.frictionCombineModeInt = 3;
-	//	constructionBitstream->Write<PxMaterialFlags>(flags);
 	}
-
 	return data;
 }
 
